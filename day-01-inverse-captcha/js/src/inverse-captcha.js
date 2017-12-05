cd69b1da986eca7577e52c5e0aaf3465607f0256
Mark Borcherding <markborcherding@gmail.com>
2017-12-04 22:55:42 -0600

    v2

--- day-01-inverse-captcha/js/src/inverse-captcha.js
+++ day-01-inverse-captcha/js/src/inverse-captcha.js
@@ -1,18 +1,34 @@
 import R from 'ramda'
 
+const log = R.tap(console.log)
+
 const first = R.map(R.head)
 
 const sum = R.sum
 
 const matching = R.filter(([a, b]) => a === b)
 
-const neighbors = ([head, ...tail]) => R.zip([head, ...tail], [...tail, head])
+const pairWith = f => list => R.zip(f(list), list)
+
+const neighbors = ([head, ...tail]) => ([...tail, head])
+
+const antipodals = (x) => {
+  const [head, tail] = R.splitAt(R.length(x) / 2)(x)
+  return [...tail, ...head]
+}
 
 const inverseCaptcha = R.compose(
   sum,
   first,
   matching,
-  neighbors,
+  pairWith(neighbors),
+)
+
+export const version2 = R.compose(
+  sum,
+  first,
+  matching,
+  pairWith(antipodals),
 )
 
 export default inverseCaptcha
