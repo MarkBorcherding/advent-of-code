77e694d4fa19bf5fa7bcb4d69a8ade5be16e9ec3
Mark Borcherding <markborcherding@gmail.com>
2017-12-14 22:35:56 -0600

    day7

--- day-07-recursive-circus/scala/day07.scala
+++ day-07-recursive-circus/scala/day07.scala
@@ -1,6 +1,5 @@
 package adventofcode
 
-
 case class Program(name: String, weight: Int, children: Seq[String] = Nil)
 
 object Project
@@ -22,18 +21,33 @@ object Day07 {
             |cntj (57)""".stripMargin
 
   val program = raw"(\w+) \((\d+)\)(?: -> (.*))?".r
-  def parse(input:String): Seq[Program] =
+  def parse(input:String) =
     input
       .split("\n")
       .map(_.trim)
       .collect {
         case program(name, weight, null) => Program(name, weight.toInt)
-        case program(name, weight, others) => Program(name, weight.toInt, others.split(",").map(_.trim))
-      }
+        case program(name, weight, others) => Program(name, weight.toInt, others.split(",").map(_.trim)) }
+      .map {  p => p.name -> p }
+      .toMap
 
   def root(programs: Seq[Program]): Program = {
     val children = programs.flatMap(_.children)
     programs.filterNot { p => children.contains(p.name) }.head
   }
 
+  def weight(programs: Map[String, Program], program: Program):Int = {
+    if(program.children.isEmpty) return program.weight
+
+    val childrenWeights =
+      program
+        .children
+        .map { c => c -> weight(programs, programs.get(c).get) }
+
+    if(childrenWeights.map{ c => c._2 }.distinct.length != 1) {
+      childrenWeights.map { c => println(List(c._2, programs.get(c._1).get.weight)) }
+    }
+
+    return program.weight + childrenWeights.map{ c => c._2 }.sum
+  }
 }
