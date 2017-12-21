be1b66115eb86224c04807d11996ab6827f93938
Mark Borcherding <markborcherding@gmail.com>
2017-12-21 10:17:13 -0600

    Stop the silly map of optional

--- day-09-stream-processing/scala/src/main/scala/Day09.scala
+++ day-09-stream-processing/scala/src/main/scala/Day09.scala
@@ -1,23 +1,21 @@
 package adventofcode
 
-import scala.annotation.tailrec
-
 object Day09 {
 
   // <>, empty garbage.
-  //   <random characters>, garbage containing random characters.
-  //   <<<<>, because the extra < are ignored.
-  //   <{!>}>, because the first > is canceled.
-  //   <!!>, because the second ! is canceled, allowing the > to terminate the garbage.
-  //   <!!!>>, because the second ! and the first > are canceled.
-  //   <{o"i!a,<{i<a>, which ends at the first >.
+  // <random characters>, garbage containing random characters.
+  // <<<<>, because the extra < are ignored.
+  // <{!>}>, because the first > is canceled.
+  // <!!>, because the second ! is canceled, allowing the > to terminate the garbage.
+  // <!!!>>, because the second ! and the first > are canceled.
+  // <{o"i!a,<{i<a>, which ends at the first >.
 
 
-  val stripNegated = "!.".r.replaceAllIn(_:CharSequence, "")
   val garbage = "<([^>]*)>".r
-  val stripGarbage = garbage.replaceAllIn(_:CharSequence, "")
   val countGarbage = garbage.findAllIn(_:CharSequence).matchData.map(_.group(1)).mkString.length()
-  val decomma = ",".r.replaceAllIn(_:CharSequence, "")
+  val stripNegated = "!.".r.replaceAllIn(_:CharSequence, "")
+  val stripGarbage = garbage.replaceAllIn(_:CharSequence, "")
+  val stripCommas = ",".r.replaceAllIn(_:CharSequence, "")
 
   def lines(input:String) = input.split("\n").map(_.trim)
 
@@ -25,17 +23,15 @@ object Day09 {
     lines(sampleInput)
       .map(go)
 
-  def go(s: String) =
-    Some(s)
-      .map(stripNegated)
-      .map(stripGarbage)
-      .map(decomma)
-      .map(score)
+  val go =
+    stripNegated andThen
+    stripGarbage andThen
+    stripCommas andThen
+    score
 
-  def count(s: String) =
-    Some(s)
-      .map(stripNegated)
-      .map(countGarbage)
+  val count =
+    stripNegated andThen
+    countGarbage
 
   def score(s: String) =
     s.foldLeft(0 -> 1) {
