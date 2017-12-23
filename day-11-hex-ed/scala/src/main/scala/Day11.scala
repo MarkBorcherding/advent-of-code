bf3eefe39dec2b49e7fe879caea2f7998e6bab82
Mark Borcherding <markborcherding@gmail.com>
2017-12-22 23:43:03 -0600

    Day 11

--- day-11-hex-ed/scala/src/main/scala/Day11.scala
+++ day-11-hex-ed/scala/src/main/scala/Day11.scala
@@ -1,2 +1,23 @@
 object Day11 {
+
+  val steps = (_:String).split(",").toSeq
+  val calc = (_:(Int, Int, Int)) match {
+    case (x,y,z) => (x.abs + y.abs + z.abs) / 2
+  }
+
+  val distance =
+    (_:Seq[String]).foldLeft(0, 0, 0) {
+      case ((x,y,z), "n")  => (x,   y+1, z-1)
+      case ((x,y,z), "ne") => (x+1, y,   z-1)
+      case ((x,y,z), "se") => (x+1, y-1, z)
+      case ((x,y,z), "s")  => (x,   y-1, z+1)
+      case ((x,y,z), "sw") => (x-1, y,   z+1)
+      case ((x,y,z), "nw") => (x-1, y+1, z)
+    }
+
+  val go =
+    steps andThen
+    distance andThen
+    calc
 }
+
