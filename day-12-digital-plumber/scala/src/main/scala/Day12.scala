bda5efe415f88bab681b734d4293d6227ddaa24f
Mark Borcherding <markborcherding@gmail.com>
2017-12-29 21:11:09 -0600

    connected

--- day-12-digital-plumber/scala/src/main/scala/Day12.scala
+++ day-12-digital-plumber/scala/src/main/scala/Day12.scala
@@ -1,3 +1,5 @@
+import scala.collection.immutable.SortedSet
+
 object Day12 {
 
   // answer 6...all but 1
@@ -10,13 +12,38 @@ object Day12 {
       |5 <-> 6
       |6 <-> 4, 5""".stripMargin
 
-  val node = raw"""(\d+) <-> (\d+(, \d+)*)""".r
+  val node = raw"""(\d+) <-> (.*)""".r
 
-  case class Node(index:Int, connected: Seq[Int])
+  type NodeList = SortedSet[Int]
+  type Graph = Map[Int, NodeList]
 
-  val splitLints = (_:String).split("\n").map(_.trim)
+  case class Node(index:Int, connected: NodeList)
+  object Node {
+    def apply(index: String, connected: String):Node =
+      Node(
+        index.toInt,
+        connected
+          .split(",")
+          .map(_.trim)
+          .map(_.toInt)
+          .foldLeft(SortedSet[Int]()){ (acc,i) =>  acc + i })
+  }
+
+  val parse =
+    (_:String)
+      .split("\n")
+      .map(_.trim)
+      .collect { case node(index, connected) => Node(index, connected) }
+      .foldLeft(Map[Int, NodeList]()) { (g, n) => g + (n.index -> n.connected) }
 
 
-
+  def connected(g: Graph, visited: NodeList = SortedSet[Int](), visiting: Int = 0):NodeList = {
+    g.get(visiting).get match {
+      case children if visited.contains(visiting) => visited
+      case children => children.foldLeft(visited) {
+        (v, i) => v ++ connected(g, v + visiting, i)
+      }
+    }
+  }
 }
 
