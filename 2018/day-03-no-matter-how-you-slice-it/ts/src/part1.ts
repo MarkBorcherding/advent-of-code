a8f83cc28784e272148ab23a9ff294c1fe018803
Mark Borcherding <markborcherding@gmail.com>
2018-12-13 10:22:52 -0600

    start day 3

--- 2018/day-03-no-matter-how-you-slice-it/ts/src/part1.ts
+++ 2018/day-03-no-matter-how-you-slice-it/ts/src/part1.ts
@@ -1,6 +1,23 @@
 import { expect } from 'chai'; 
 
+interface Matrix<T> extends Array<Array<T>> {}
 
+const always = <T>(t:T) => <A extends any[]>(...a:A) => t
+
+
+const matrix = <T>(width: number, height: number, val: (x:number, y:number) => T):Matrix<T> =>
+    [ ...Array(height).fill(0).map((_,y) =>
+           [...Array(width).fill(0).map((_,x) => val(x,y))])]
+
+const repeat = (n:number) => <T>(f:(i:number) => T) =>
+    [...Array(n).keys()].map(f)
+
+const matrix2 = <T>(width: number, height: number, val: (x:number, y:number) => T):Matrix<T> =>
+    repeat (height) ((y) => repeat (width) ((x) => val(x,y)))
+
+const solve = (s:string) => {
+    return matrix(3,2,always(0))
+}
 
 describe('Part 1', () => {
     it('should work', () => {
