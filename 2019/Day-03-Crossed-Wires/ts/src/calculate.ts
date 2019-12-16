e93aca01d4571f7331c131740728d9c59a203260
Mark Borcherding <markborcherding@gmail.com>
2019-12-16 10:52:05 -0600

    switch to immutable

--- 2019/Day-03-Crossed-Wires/ts/src/calculate.ts
+++ 2019/Day-03-Crossed-Wires/ts/src/calculate.ts
@@ -1,3 +1,4 @@
+import { Map } from "immutable";
 import { pipe, map, split, tap, range, min, max } from "@lib/f";
 
 /** All possible directions */
@@ -6,139 +7,209 @@ const Direction = {
   Down: "D",
   Left: "L",
   Right: "R"
-} as const
+} as const;
 
 /** A list of the possible values that we could parse */
-const Directions = Object.values(Direction)
+const Directions = Object.values(Direction);
 
 /** The direction of the wire placement */
-type Direction = typeof Direction[keyof typeof Direction]
+type Direction = typeof Direction[keyof typeof Direction];
 
-type Length = number
+type Length = number;
 
 /** A single instruction on the list of instructions for a single wire */
-type WireCommandInstruction = [Direction, Length]
+type WireCommandInstruction = [Direction, Length];
 
 /** All the instructions for a single wire */
-type WireSchematic = WireCommandInstruction[]
-
-/** A Readonly only hashmap */
-type Map<T> = Readonly<{ [index: string]: T }>
+type WireSchematic = WireCommandInstruction[];
 
 /** How wire segments appear on the circuit board */
-type WireSegment = "|" | "-" | "+" | undefined
+type WireSegment = "|" | "-" | "+" | "O";
 
 /** A list of all placed wires */
-type CircuitBoard = Map<Map<WireSegment>>
+type CircuitBoard = Map<number, Map<number, WireSegment>>;
 
 /** A position in x,y coordinates */
-type Position = [number, number]
+type Position = Readonly<[number, number]>;
 
-/** Split a single string into unparesed command instructions */
-const splitCommands = split(",")
+/** Split a single string into un-parsed command instructions */
+const splitCommands = split(",");
 
 /** Split a single instruction into direction and length */
-const splitIntoParts = ([direction, ...length]: string): [string, string] =>
-  [direction, length.join('')]
+const splitIntoParts = ([direction, ...length]: string): [string, string] => [
+  direction,
+  length.join("")
+];
 
 /** Type guard against any possible string when we want only a Direction */
-const validDirection = (d: string): d is Direction => Directions.indexOf(d as any) > -1
+const validDirection = (d: string): d is Direction =>
+  Directions.indexOf(d as any) > -1;
 
 /** Change the [string, string] potentiation command into a properly typed command */
-const validateParts = ([direction, length]: [string, string]): [Direction, number] => {
-  if (!validDirection(direction)) { throw new Error("Unknown direction: " + direction) }
-  return [direction, parseInt(length)]
-}
+const validateParts = ([direction, length]: [string, string]): [
+  Direction,
+  number
+] => {
+  if (!validDirection(direction)) {
+    throw new Error("Unknown direction: " + direction);
+  }
+  return [direction, parseInt(length)];
+};
 
 /** Change a single string into a WireSchematic */
 const parseIntoWire = pipe(
   splitCommands,
-  map(
-    pipe(
-      splitIntoParts,
-      validateParts)))
+  map(pipe(splitIntoParts, validateParts))
+);
 
 /** Return the new position when moving a specific direction from a starting point */
 const move = ([x, y]: Position, direction: Direction): Position => {
   switch (direction) {
-    case "D": return [x, y - 1];
-    case "U": return [x, y + 1];
-    case "R": return [x + 1, y];
-    case "L": return [x - 1, y];
+    case "D":
+      return [x, y - 1];
+    case "U":
+      return [x, y + 1];
+    case "R":
+      return [x + 1, y];
+    case "L":
+      return [x - 1, y];
   }
-}
+};
 
 /** Change the value of a single position in a 2d map  */
-const set = ([x, y]: Position, s: string, board: CircuitBoard): CircuitBoard => {
-  if (board[x] === undefined) return { ...board, [x]: { [y]: s } }
-  return { ...board, [x]: { ...board[x], [y]: s } }
-}
+const set = (
+  [x, y]: Position,
+  s: WireSegment,
+  board: CircuitBoard
+): CircuitBoard => {
+  try {
+    const rowX = board.get(x);
+    return rowX ? board.set(x, rowX.set(y, s)) : board.set(x, Map([[y, s]]));
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+/** Get the wire segment at a position */
+const get = ([x, y]: Position) => (board: CircuitBoard) => {
+  try {
+    const rowX = board.get(x);
+    return rowX ? rowX.get(y) : undefined;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
 
 /** Determine if direction is up or down to help layout the wire */
-const isVertical = (d: Direction) => d === "U" || d === "D"
+const isVertical = (d: Direction) => d === "U" || d === "D";
 
-/** Add a single wire segement to the board, starting at a specific position  */
-const placeSegment = ([board, [x, y]]: [CircuitBoard, Position], [direction, length]: WireCommandInstruction): [CircuitBoard, Position] => {
-  const positions = range(length).reduce(([head, ...rest]: Position[], curr) => {
-    return [move(head, direction), head, ...rest]
-  }, [[x, y]])
+/** The display of a wire for the given direction */
+const wireForDirection = (d: Direction) => (isVertical(d) ? "-" : "|");
+
+/** Add a single wire segment to the board, starting at a specific position  */
+const placeSegment = (
+  [board, startingPos]: [CircuitBoard, Position],
+  [direction, length]: WireCommandInstruction
+): [CircuitBoard, Position] => {
+  const rest = range(length);
+  const positions = rest.reduce(([head, ...rest]) => {
+    return head === undefined
+      ? [move(startingPos, direction)]
+      : [move(head, direction), head, ...rest];
+  }, [] as Position[]);
 
   const newBoard = positions.reduce((board, [x, y]) => {
-    if(x === 0 && y === 0) return set([0,0], "O", board)
-    switch (board[x] && board[x][y]) {
-      case undefined: return set([x, y], isVertical(direction) ? "-" : "|", board)
-      default: return set([x, y], "+", board)
+    if (x === 0 && y === 0) return set([0, 0], "O", board);
+    switch (get([x, y])(board)) {
+      case undefined:
+        return set([x, y], wireForDirection(direction), board);
+      case "O":
+        return board;
+      default:
+        return set([x, y], "+", board);
     }
-  }, board)
+  }, board);
 
-  const [newPos, ...rest] = positions
-  return [newBoard, newPos]
-}
+  const [newPos] = positions;
+  return [newBoard, newPos];
+};
 
 /** Add all the segments for a wire starting at the origin */
-const layoutWire = ((board: CircuitBoard, wire: WireSchematic): CircuitBoard => {
-  const [b] = wire.reduce(placeSegment, [board, [0, 0]])
-  return b
-})
+const layoutWire = (board: CircuitBoard, wire: WireSchematic): CircuitBoard => {
+  const [b] = wire.reduce(placeSegment, [board, origin]);
+  return b;
+};
 
-/** Determin the bounds of the circuit board */
+/** Determine the bounds of the circuit board */
 const bounds = (board: CircuitBoard) => {
-  const allX = Object.keys(board).map(parseInt)
-  const allY = Object.values(board).map(column => Object.keys(column)).reduce((acc, curr) => [...curr, ...acc], []).map(parseInt)
+  const allX = Array.from(board.keys());
+  const allY = allX.reduce((acc: number[], currX: number) => {
+    const col = board.get(currX);
+    if (col === undefined) return acc;
+    const colY = Array.from(col.keys());
+    return [...acc, ...colY];
+  }, []);
 
-  const minX = min(allX)
-  const maxX = max(allX)
-  const minY = min(allY)
-  const maxY = max(allY)
-  return [minX, maxX, minY, maxY]
-}
+  const minX = min(allX);
+  const maxX = max(allX);
+  const minY = min(allY);
+  const maxY = max(allY);
+  return [minX, maxX, minY, maxY];
+};
 
-/** Returns true if we are at the origin */
-const isOrigin = ([x,y]: Position) => x === 0 && y === 0
+/** The starting point for all circuit board */
+const EmptyCircuitBoard: CircuitBoard = Map([[0, Map([[0, "O"]])]]);
+
+/** The origin coordinates */
+const origin: Position = [0, 0] as const;
 
 /** Create a visual representation of the circuit board */
 const display = (board: CircuitBoard) => {
-  const [minX, maxX, minY, maxY] = bounds(board)
-  const yRange = range(maxY - minY, minY)
-  const xRange = range(maxX - minX, minX)
-  return xRange.map(x =>
-    yRange.map(y =>
-      isOrigin([x,y]) ? 'O' : board[x] && board[x][y] || ' ' 
-    ).join("")
-  ).join("\n")
-}
+  const [minX, maxX, minY, maxY] = bounds(board);
+  const yRange = range(maxY - minY, minY);
+  const xRange = range(maxX - minX, minX);
+  const columns = yRange.map(y =>
+    xRange
+      .map(x => {
+        const currY = get([x, y])(board);
+        return currY === undefined ? " " : currY;
+      })
+      .join("")
+  );
+  return columns.join("\n");
+};
+
+const filter = (f: (segment: WireSegment) => boolean) => (
+  board: CircuitBoard
+): Position[] =>
+  board.reduce(
+    (xAcc, curr, x) =>
+      curr.reduce(
+        (yAcc, seg, y) => (f(seg) ? [...yAcc, [x, y] as Position] : yAcc),
+        xAcc
+      ),
+    [] as Position[]
+  );
+
+const allIntersections = filter(s => s === "+");
+
+const manhattanDistance = ([x, y]: Position) => Math.abs(x) + Math.abs(y);
+
+const closestIntersection = (board: CircuitBoard) => {
+  return allIntersections(board).reduce((acc, cur) => {
+    if (acc === undefined) return cur;
+    if (manhattanDistance(cur) < manhattanDistance(acc)) return cur;
+    return acc;
+  });
+};
 
 /** Calculate the closest intersect */
 const calculate = (input: string[]) => {
-  const wires = map(
-    parseIntoWire,
-  )(input)
+  const wires = map(parseIntoWire)(input);
+  const circuitBoard = wires.reduce(layoutWire, EmptyCircuitBoard);
+  return manhattanDistance(closestIntersection(circuitBoard));
+};
 
-  const circuitBoard = wires.reduce(layoutWire, {})
-
-  console.log(display(circuitBoard))
-
-  return 1;
-}
-
-export default calculate
+export default calculate;
--- 2019/Day-03-Crossed-Wires/ts/src/test.ts
+++ 2019/Day-03-Crossed-Wires/ts/src/test.ts
@@ -1,28 +1,27 @@
-#!/usr/bin/env npx ts-node -r tsconfig-paths/register
+#!/usr/bin/env npx mocha --require ts-node/register -r tsconfig-paths/register
 
-import calculate from './calculate'
-import assert from 'assert'
-import { describe, it } from '@lib/test'
+import calculate from "./calculate";
+import assert from "assert";
+import { describe, it } from "@lib/test";
 
 describe("Example Data")(() => {
   it("example 1")(() => {
-    assert.deepEqual(
+    assert.equal(
       159,
       calculate([
         "R75,D30,R83,U83,L12,D49,R71,U7,L72",
         "U62,R66,U55,R34,D71,R55,D58,R83"
       ])
-    )
-  })
+    );
+  });
 
   it("example 2")(() => {
-    assert.deepEqual(
+    assert.equal(
       135,
       calculate([
         "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
         "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
       ])
-    )
-  })
-})
-
+    );
+  });
+});
