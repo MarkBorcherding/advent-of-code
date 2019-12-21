51b70cef13deb6a3485cf27a1137b9d4c62a4508
Mark Borcherding <markborcherding@gmail.com>
2019-12-21 08:40:23 -0600

    start extracting reusable bits

--- 2019/Day-04-Secure-Container/ts/src/calculate.ts
+++ 2019/Day-04-Secure-Container/ts/src/calculate.ts
@@ -1,25 +1,4 @@
-import { pipe } from "@lib/f";
-
-function* range(min: number, max: number): Iterator<number> {
-  for (let i = min; i <= max; i++) {
-    yield i;
-  }
-}
-
-const reduce = <T, ACC>(f: (acc: ACC, cur: T) => ACC) => (starting: ACC) => (
-  iter: Iterator<T>
-) => {
-  let acc = starting;
-  let cur = iter.next();
-  while (!cur.done) {
-    acc = f(acc, cur.value);
-    cur = iter.next();
-  }
-  return acc;
-};
-
-const all = <T>(...f: ((t: T) => boolean)[]) => (t: T) =>
-  f.reduce((acc, cur) => acc && cur(t), true);
+import { groupBy, all, filter, rangeIterator } from "@lib/f";
 
 const digitsIncrease = (s: number[]) =>
   s.reduce((acc, cur, index, arr) => {
@@ -27,18 +6,6 @@ const digitsIncrease = (s: number[]) =>
     if (prevIndex < 0) return acc;
     return acc && arr[prevIndex] <= cur;
   }, true);
-
-const allButLast = <T>(t: T[]) => t.slice(0, -1);
-const last = <T>(t: T[]) => t[t.length - 1];
-
-const groupBy = <A>(f: (cur: A, index: number, arr: A[]) => boolean) => (
-  a: A[]
-) =>
-  a.reduce((acc: Array<A[]>, cur, index, arr) => {
-    if (index === 0) return [...acc, [cur]];
-    if (f(cur, index, arr)) return [...allButLast(acc), [...last(acc), cur]];
-    return [...acc, [cur]];
-  }, []);
 
 const consecutive = (n: number) => <T>(t: T[]) => {
   const groups = groupBy((cur, index, arr: T[]) => cur === arr[index - 1])(t);
@@ -53,17 +20,10 @@ const isPossible = (n: number) => {
   return all(digitsIncrease, consecutive(2))(digits);
 };
 
-const filter = <T>(f: (t: T) => boolean) => (starting: T[] = []) => (
-  iterator: Iterator<T>
-) =>
-  reduce<T, T[]>((acc, cur) => (f(cur) ? [...acc, cur] : acc))(starting)(
-    iterator
-  );
-
 export const calculate = (s: string) => {
   const [min, max] = s.split("-").map(s => parseInt(s));
 
-  const possibles = filter(isPossible)([])(range(min, max));
+  const possibles = filter(isPossible)([])(rangeIterator(min, max));
   return possibles.length;
 };
 
--- 2019/ts/lib/f.ts
+++ 2019/ts/lib/f.ts
@@ -1,22 +1,21 @@
 type F<A, B> = (a: A) => B;
 
-export const contains = <T>(f: F<T, boolean>) => (arrayLike: ArrayLike<T>) => {
-  for (let i = 0; i < arrayLike.length; i++) {
-    if (f(arrayLike[i])) return true;
-  }
-  return false;
-};
+
+export const set = (arr: number[], index: number, val: number) => {
+    const f = [...arr];
+    f[index] = val;
+    return f
+}
 
 export const range = (count: number, startAt: number = 0) =>
   Array.from(new Array(count), (x, i) => i + startAt);
 
-export const min = (arr: number[]) => {
-  return arr.reduce((min, cur) => (cur < min ? cur : min), arr[0]);
-};
+export function* rangeIterator(min: number, max: number): Iterator<number> {
+  for (let i = min; i <= max; i++) {
+    yield i;
+  }
+}
 
-export const max = (arr: number[]) => {
-  return arr.reduce((max, cur) => (cur > max ? cur : max), arr[0]);
-};
 
 export function pipe<T1, T2, T3>(f1: F<T1, T2>, f2: F<T2, T3>): F<T1, T3>;
 export function pipe<T1, T2, T3, T4>(
@@ -35,4 +34,50 @@ export const tap = <T>(f: F<T, void>) => (val: T) => {
 
 export const map = <A, B>(f: F<A, B>) => (a: A[]): B[] => a.map(f);
 
+export const filter = <T>(f: (t: T) => boolean) => (starting: T[] = []) => (
+  iterator: Iterator<T>
+) =>
+  reduce<T, T[]>((acc, cur) => (f(cur) ? [...acc, cur] : acc))(starting)(
+    iterator
+  );
+
+export const reduce = <T, ACC>(f: (acc: ACC, cur: T) => ACC) => (starting: ACC) => (
+  iter: Iterator<T>
+) => {
+  let acc = starting;
+  let cur = iter.next();
+  while (!cur.done) {
+    acc = f(acc, cur.value);
+    cur = iter.next();
+  }
+  return acc;
+};
+
 export const split = (delim: string) => (val: string) => val.split(delim);
+
+export const all = <T>(...f: ((t: T) => boolean)[]) => (t: T) =>
+  f.reduce((acc, cur) => acc && cur(t), true);
+
+export const allButLast = <T>(t: T[]) => t.slice(0, -1);
+
+export const last = <T>(t: T[]) => t[t.length - 1];
+
+export const groupBy = <A>(f: (cur: A, index: number, arr: A[]) => boolean) => (
+  a: A[]
+) =>
+  a.reduce((acc: Array<A[]>, cur, index, arr) => {
+    if (index === 0) return [...acc, [cur]];
+    if (f(cur, index, arr)) return [...allButLast(acc), [...last(acc), cur]];
+    return [...acc, [cur]];
+  }, []);
+
+export const min = (arr: number[]) => arr.reduce((min, cur) => (cur < min ? cur : min), arr[0]);
+
+export const max = (arr: number[]) => arr.reduce((max, cur) => (cur > max ? cur : max), arr[0]);
+
+export const contains = <T>(f: F<T, boolean>) => (arrayLike: ArrayLike<T>) => {
+  for (let i = 0; i < arrayLike.length; i++) {
+    if (f(arrayLike[i])) return true;
+  }
+  return false;
+};
