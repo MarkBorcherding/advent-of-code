99412375d5fc3f7abf347fa406f76195c5e15166
Mark Borcherding <mark.borcherding@wwt.com>
2021-11-30 23:54:04 -0600

    day1

--- 2021/advent-of-code-2021-kt/src/main/kotlin/Day01.kt
+++
@@ -1,4 +0,0 @@
-
-class Day01 {
-     fun Doit(a: Int, b: Int):Int { return a + b }
-}
--- 2021/advent-of-code-2021-kt/src/test/kotlin/ExampleTest.kt
+++ 2021/advent-of-code-2021-kt/src/test/kotlin/ExampleTest.kt
@@ -1,11 +1,10 @@
+
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 internal class ExampleTest {
 
     @Test
-    fun testSum() {
+    fun example() {
         val expected = 42
-        assertEquals(expected, Day01().Doit(41,1))
     }
 }
