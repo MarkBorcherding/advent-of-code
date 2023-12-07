3cbe5ebbe2519277eba08716102445a468d5ba5c
Mark Borcherding <mark.borcherding@wwt.com>
2023-12-07 17:28:39 -0600

    day 3 part 1

--- 2023/day03/part1.py
+++ 2023/day03/part1.py
@@ -1,3 +1,4 @@
+import io
 import math
 import re
 
@@ -15,16 +16,47 @@ input = """
 ...$.*....
 .664.598..
 """.strip()
+f = io.open('./input.txt')
+input = f.read()
+
+rows = input.splitlines()
+
+number_regex = re.compile(r"([0-9]+)")
+
+symbols = set()
+symbol_regex = re.compile(r"[^0-9.]")
+
+for idx, row in enumerate(rows):
+    s = set(list(map(lambda x: (idx, x.start()), symbol_regex.finditer(row))))
+    symbols = symbols | s
+
+def neighbors(row: int, start: int, end: int) -> set:
+    width = (range(start - 1, end + 1 ))
+
+    n = set()
+    for x in width:
+        n.add((row - 1, x))
+        n.add((row + 1, x))
+
+    n.add((row, min(width)))
+    n.add((row, max(width)))
+    return n
+
+
+hits = []
+for idx, row in enumerate(rows):
+    for match in number_regex.finditer(row):
+        (start, end) = match.span()
+        n = neighbors(idx, start, end)
+        hit = len(symbols.intersection(n))
+        s = int(match.group(0))
+        if hit > 0:
+            print(f'part: {s}')
+            hits.append(s)
+        else:
+            print(f"not part {s}")
 
 
 
-class Schematic:
 
-    def __init__(self, input: str):
-       np.arange
-
-
-s = Schematic(input)
-
-print(s.matrix.to_s())
-print(s.symbols())
+print(sum(hits))
