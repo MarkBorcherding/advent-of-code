57f98dd46edc5e6f7f718430fe3d71497eb8aa13
Mark Borcherding <mark.borcherding@wwt.com>
2023-12-08 21:59:41 -0600

    day 4 part 2

--- 2023/day04/part1.py
+++ 2023/day04/part1.py
@@ -12,26 +12,58 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 """.strip()
 
 f = open("./input.txt")
+
+
 input = f.read().strip()
 
 
-regex = re.compile(r"Card\s+\d+: ([^|]*) \|(.*)")
-cards = list(map(lambda x: regex.findall(x), input.splitlines()))
+class Card:
+    line_regex = re.compile(r"Card\s+\d+: ([^|]*) \|(.*)")
+    numbers_regex = re.compile(r'(\d+)')
 
-numbers = re.compile(r'(\d+)')
-scores = []
-for [(winning_numbers, my_card)] in cards:
-    a = set(numbers.findall(winning_numbers))
-    b = set(numbers.findall(my_card))
+    @staticmethod
+    def from_line(line: str):
+        [(winning, mine)] = Card.line_regex.findall(line)
+        return Card(winning, mine)
 
-    common = a.intersection(b)
-    matches = len(common)
-
-    scores.append(functools.reduce(lambda acc, cur: (1 if cur == 0 else acc * 2), range(0,matches), 0))
-
-
+    def __init__(self, winning_numbers, my_numbers):
+        self.winning_numbers = set(Card.numbers_regex.findall(winning_numbers))
+        self.my_numbers = set(Card.numbers_regex.findall(my_numbers))
+        self.matches = self.winning_numbers.intersection(self.my_numbers)
 
 
+cards = list(map(lambda x: Card.from_line(x), input.splitlines()))
+
+totals: dict[int, int] = {}
 
 
-print(sum(scores))
+def tally(index: int):
+    totals[index] = totals.get(index, 0) + 1
+
+
+def print_tallies():
+    for k, v in totals.items():
+        print(k + 1, v)
+
+
+def score(index: int, depth=0):
+    copies = len(cards[index].matches)
+
+    if depth == 0:
+        tally(index)
+
+    next_cards = range(index + 1, index + 1 + copies)
+
+    for c in next_cards:
+        tally(c)
+        score(c, depth + 1)
+
+
+for idx, card in enumerate(cards):
+    score(idx)
+
+total = 0
+for k, v in totals.items():
+    total += v
+
+print(total)
